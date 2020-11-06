db144af8d0d5299a91ffe7f4abdd0e7a38cbdccd
Tweetyas <anastasiabilevic@gmail.com>
2020-11-06 19:22:20 +0700

    Task_1
    Task_1

--- auth.cpp
+++ auth.cpp
@@ -1,12 +1,14 @@
 #include "auth.h"
 #include "ui_auth.h"
 #include <QIcon>
+
 auth::auth(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::auth)
 {
     ui->setupUi(this);
-    setWindowIcon(QIcon("./logo.ico"));
+    setWindowIcon(QIcon(":/source/icon.ico"));
+
 }
 
 auth::~auth()
